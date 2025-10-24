804cfcca3eb0600639fd7f0ad8a7aa3f60a9d442
Kosala123 <kosalakumara2001@gmail.com>
2025-10-24 22:42:29 +0530

    Updated AWS Backup Part

--- utils/aws.js
+++ utils/aws.js
@@ -37,7 +37,11 @@
 
 // module.exports = { uploadToS3 };
 
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -49,12 +53,22 @@ const s3 = new S3Client({
   },
 });
 
+// Convert stream to string
+const streamToString = (stream) =>
+  new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.on("error", reject);
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf-8")));
+  });
+
 /**
  * Upload backup data to S3
  * @param {Object} data - The backup object to upload
  * @param {String} fileName - The unique file name for this upload
  * @param {String} electionName - The folder name (like "presidential-2025")
  */
+
 const uploadToS3 = async (data, fileName, electionName) => {
   try {
     const params = {
@@ -71,6 +85,30 @@ const uploadToS3 = async (data, fileName, electionName) => {
   } catch (err) {
     console.error("❌ S3 Upload failed:", err);
   }
+
+  /**
+   * Read (download) a JSON file from S3 and convert it to an object
+   * @param {String} fileName - The file name without extension
+   * @param {String} electionName - Folder name
+   */
+
+  const redFromS3 = async (fileName, electionName) => {
+    try {
+      const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${electionName}/${fileName}.json`,
+      };
+
+      const command = new GetObjectCommand(params);
+      const data = await s3.send(command);
+
+      const fileContent = await streamToString(data.Body);
+      console.log(`📥 Downloaded file from S3: ${fileName}.json`);
+      return JSON.parse(fileContent);
+    } catch (err) {
+      console.error("❌ S3 Read failed:", err);
+    }
+  };
 };
 
 module.exports = { uploadToS3 };
